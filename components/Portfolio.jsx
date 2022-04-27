bf40c51d60ae1638d6d6fc705a09714d2db4e497
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-04-27 18:36:16 +0530

    erroe fixed

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div classNameName=" container xl:px-8 xl:py-10 pb-20">
-      <div classNameName="flex">
-        <div classNameName="mx-4 w-full">
-          <div classNameName="mx-auto mb-[60px] max-w-[620px] text-center lg:mb-20">
-            <span classNameName="mb-2 block text-lg font-semibold text-primary">
+    <div className=" container xl:px-8 xl:py-10 pb-20">
+      <div className="flex">
+        <div className="mx-4 w-full">
+          <div className="mx-auto mb-[60px] max-w-[620px] text-center lg:mb-20">
+            <span className="mb-2 block text-lg font-semibold text-primary">
               Portfolio
             </span>
-            <h2 classNameName="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[42px]">
+            <h2 className="mb-4 text-3xl font-bold text-dark sm:text-4xl md:text-[42px]">
               Our Designs
             </h2>
-            <p classNameName="text-lg leading-relaxed text-body-color sm:text-xl sm:leading-relaxed">
+            <p className="text-lg leading-relaxed text-body-color sm:text-xl sm:leading-relaxed">
               Dont take our word for it... Read what our customers feel about
               our products!
             </p>
@@ -21,7 +21,7 @@ const Portfolio = () => {
       </div>
       <div className="container grid xl:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-4 ">
         <div className="   text-gray-900">
-          <div classNameName=" mt-2">
+          <div className=" mt-2">
             <img
               src="/images/portfolio/product1.png"
               alt=" random imgee"
@@ -38,7 +38,7 @@ const Portfolio = () => {
         </div>
 
         <div className="   text-gray-900">
-          <div classNameName=" mt-2">
+          <div className=" mt-2">
             <img
               src="/images/portfolio/product1.png"
               alt=" random imgee"
@@ -54,7 +54,7 @@ const Portfolio = () => {
           </div>
         </div>
         <div className="   text-gray-900">
-          <div classNameName=" mt-2">
+          <div className=" mt-2">
             <img
               src="/images/portfolio/product2.png"
               alt=" random imgee"
