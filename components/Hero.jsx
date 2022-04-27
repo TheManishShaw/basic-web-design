65def4b2d7de725ef62a63e90b037acd40fb112e
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-04-27 11:51:11 +0530

    heading change to uppercase

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,8 +13,8 @@ const Hero = () => {
               className="hero-content wow fadeInUp mx-auto max-w-[780px] text-center"
               data-wow-delay=".2s"
             >
-              <h1 className="mb-8 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug md:text-[45px] md:leading-snug">
-                Upscale your business website with trendsetter tools of website
+              <h1 className="mb-8 text-3xl font-bold leading-snug text-white sm:text-4xl sm:leading-snug md:text-[45px] md:leading-snug uppercase">
+                Upscale your Business Website with Trendsetter Tools of website
                 and app development
               </h1>
               <p className="mx-auto mb-10 max-w-[600px] text-base text-[#e4e4e4] sm:text-lg sm:leading-relaxed md:text-xl md:leading-relaxed">
