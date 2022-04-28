ee3b9192e7c9c2be4cecf0ac497941f3e95b7e33
Manish Shaw <80380640+TheManishShaw@users.noreply.github.com>
2022-04-28 18:20:57 +0530

    contact

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,67 +3,71 @@ import React from 'react'
 const Contact = () => {
   return (
     <section id="contact" className="relative py-15 md:py-[80px]">
-      <div className="absolute top-0 left-0 z-[-1] h-1/2 w-full bg-[#f3f4fe] lg:h-[45%] xl:h-1/2"></div>
       <div className="container px-4">
-        <div className="-mx-4 flex flex-wrap items-center">
-          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+        <div className="-mx-4 flex flex-wrap items-center ">
+          <div className="w-full mb-28 px-4 lg:w-7/12 xl:w-8/12">
             <div className="ud-contact-content-wrapper">
               <div className="ud-contact-title mb-12 lg:mb-[150px]">
-                <span className="mb-5 text-base font-semibold text-dark">
-                  CONTACT US
-                </span>
-                <h2 className="text-[35px] font-semibold">
-                  Lets talk about <br />
+                <h2 className="text-[35px] font-semibold pb-7">
+                  Lets talk about
+                  <br />
                   Love to hear from you!
                 </h2>
-              </div>
-              <div className="mb-12 flex flex-wrap justify-between lg:mb-0">
-                <div className="mb-8 flex w-[330px] max-w-full">
-                  <div className="mr-6 text-[32px] text-primary">
-                    <svg
-                      width="29"
-                      height="35"
-                      viewBox="0 0 29 35"
-                      className="fill-current"
-                    >
-                      <path d="M14.5 0.710938C6.89844 0.710938 0.664062 6.72656 0.664062 14.0547C0.664062 19.9062 9.03125 29.5859 12.6406 33.5234C13.1328 34.0703 13.7891 34.3437 14.5 34.3437C15.2109 34.3437 15.8672 34.0703 16.3594 33.5234C19.9688 29.6406 28.3359 19.9062 28.3359 14.0547C28.3359 6.67188 22.1016 0.710938 14.5 0.710938ZM14.9375 32.2109C14.6641 32.4844 14.2812 32.4844 14.0625 32.2109C11.3828 29.3125 2.57812 19.3594 2.57812 14.0547C2.57812 7.71094 7.9375 2.625 14.5 2.625C21.0625 2.625 26.4219 7.76562 26.4219 14.0547C26.4219 19.3594 17.6172 29.2578 14.9375 32.2109Z" />
-                      <path d="M14.5 8.58594C11.2734 8.58594 8.59375 11.2109 8.59375 14.4922C8.59375 17.7188 11.2187 20.3984 14.5 20.3984C17.7812 20.3984 20.4062 17.7734 20.4062 14.4922C20.4062 11.2109 17.7266 8.58594 14.5 8.58594ZM14.5 18.4297C12.3125 18.4297 10.5078 16.625 10.5078 14.4375C10.5078 12.25 12.3125 10.4453 14.5 10.4453C16.6875 10.4453 18.4922 12.25 18.4922 14.4375C18.4922 16.625 16.6875 18.4297 14.5 18.4297Z" />
-                    </svg>
+                <div className="mb-12 flex flex-wrap justify-between lg:mb-0">
+                  <div className="mb-8 flex w-[330px] max-w-full">
+                    <div className="mr-6 text-[32px] text-primary">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+                        />
+                      </svg>
+                    </div>
+                    <div>
+                      <h5 className="mb-6 text-xl font-semibold">Call Us</h5>
+                      <p className="text-lg text-body-color font-semibold">
+                        <a href="tel:+44 7947772257"> +44 7947772257 </a>
+                      </p>
+                      <p className="text-lg text-body-color font-semibold">
+                        <a href="tel:+91-7777907777">+91-7777907777</a>
+                      </p>
+                    </div>
                   </div>
-                  <div>
-                    <h5 className="mb-6 text-lg font-semibold">Our Location</h5>
-                    <p className="text-base text-body-color">
-                      Judges Bunglow Rd, Suryapooja Block B, Satellite,
-                      Ahmedabad, Gujarat 380015
-                    </p>
-                  </div>
-                </div>
-                <div className="mb-8 flex w-[330px] max-w-full">
-                  <div className="mr-6 text-[32px] text-primary">
-                    <svg
-                      width="34"
-                      height="25"
-                      viewBox="0 0 34 25"
-                      className="fill-current"
-                    >
-                      <path d="M30.5156 0.960938H3.17188C1.42188 0.960938 0 2.38281 0 4.13281V20.9219C0 22.6719 1.42188 24.0938 3.17188 24.0938H30.5156C32.2656 24.0938 33.6875 22.6719 33.6875 20.9219V4.13281C33.6875 2.38281 32.2656 0.960938 30.5156 0.960938ZM30.5156 2.875C30.7891 2.875 31.0078 2.92969 31.2266 3.09375L17.6094 11.3516C17.1172 11.625 16.5703 11.625 16.0781 11.3516L2.46094 3.09375C2.67969 2.98438 2.89844 2.875 3.17188 2.875H30.5156ZM30.5156 22.125H3.17188C2.51562 22.125 1.91406 21.5781 1.91406 20.8672V5.00781L15.0391 12.9922C15.5859 13.3203 16.1875 13.4844 16.7891 13.4844C17.3906 13.4844 17.9922 13.3203 18.5391 12.9922L31.6641 5.00781V20.8672C31.7734 21.5781 31.1719 22.125 30.5156 22.125Z" />
-                    </svg>
-                  </div>
-                  <div>
-                    <h5 className="mb-6 text-lg font-semibold">
-                      How Can We Help?
-                    </h5>
-                    <p className="text-base text-body-color">
-                      sales@techblic.com
-                    </p>
-                    <p className="text-base text-body-color">
-                      sales@techblic.com
-                    </p>
+                  <div className="mb-8 flex w-[330px] max-w-full">
+                    <div className="mr-6 text-[32px] text-primary">
+                      <svg
+                        width="34"
+                        height="25"
+                        viewBox="0 0 34 25"
+                        className="fill-current"
+                      >
+                        <path d="M30.5156 0.960938H3.17188C1.42188 0.960938 0 2.38281 0 4.13281V20.9219C0 22.6719 1.42188 24.0938 3.17188 24.0938H30.5156C32.2656 24.0938 33.6875 22.6719 33.6875 20.9219V4.13281C33.6875 2.38281 32.2656 0.960938 30.5156 0.960938ZM30.5156 2.875C30.7891 2.875 31.0078 2.92969 31.2266 3.09375L17.6094 11.3516C17.1172 11.625 16.5703 11.625 16.0781 11.3516L2.46094 3.09375C2.67969 2.98438 2.89844 2.875 3.17188 2.875H30.5156ZM30.5156 22.125H3.17188C2.51562 22.125 1.91406 21.5781 1.91406 20.8672V5.00781L15.0391 12.9922C15.5859 13.3203 16.1875 13.4844 16.7891 13.4844C17.3906 13.4844 17.9922 13.3203 18.5391 12.9922L31.6641 5.00781V20.8672C31.7734 21.5781 31.1719 22.125 30.5156 22.125Z" />
+                      </svg>
+                    </div>
+                    <div>
+                      <h5 className="mb-6 text-xl font-semibold">
+                        How Can We Help?
+                      </h5>
+                      <p className="text-lg text-body-color font-semibold">
+                        <a href="mailto:sales@techblic.com">
+                          sales@techblic.com
+                        </a>
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+
           <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
             <div
               className="wow fadeInUp rounded-lg bg-white py-10 px-8 shadow-testimonial sm:py-12 sm:px-10 md:p-[60px] lg:p-10 lg:py-12 lg:px-10 2xl:p-[60px]"
@@ -121,6 +125,16 @@ const Contact = () => {
               </form>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="absolute bottom-0 left-0 z-[-1] h-1/2 w-full bg-[#f3f4fe] lg:h-[4%] xl:h-1/2">
+        <div className="absolute inset-0 bg-gray-300">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8733.492659373163!2d72.53175672442777!3d23.024457531876713!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e8555282e0cbf%3A0x21347c8e76fbf474!2sTecblic%20Private%20Limited!5e0!3m2!1sen!2sin!4v1651147293578!5m2!1sen!2sin"
+            width="100%"
+            height="100%"
+            loading="lazy"
+          ></iframe>
         </div>
       </div>
     </section>
